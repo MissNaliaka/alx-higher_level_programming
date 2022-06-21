bd3b0282497f620087c4807289b077969cf28186
MissNaliaka <naliakawabuke@gmail.com>
2022-06-21 18:34:12 +0000

    updated

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Define a class Square."""
+"""Defines a class Square"""
 
 
- class Square:
-     """Represent a square."""
-     pass
+class Square:
+    """An empty Square class"""
+    pass
